8368dec5f5e77a5cdf8ddbd605d992dec0ab4132
Greg Havekes <greghavekes@gmail.com>
2017-07-23 17:56:19 -0400

    Add check to build only for a new revision

--- build/build.sh
+++ build/build.sh
@@ -5,26 +5,28 @@ INSTANCE=$1
 REVISION=$2
 install_dir=../servers/$INSTANCE/r$REVISION
 
-# Download revision and clean
-if [[ -d "trunk" ]]; then
-  echo "Updating to r$REVISION..."
-  cd trunk
-  svn up -r $REVISION
-  make clean
-else
-  "Cloning r$REVISION..."
-  svn co --username anon -r $REVISION svn://servers.simutrans.org/simutrans/trunk
-  cd trunk
+if [[ $REVISION -nq $(cat last_build.revision) ]]; then
+  # Download revision and clean
+  if [[ -d "trunk" ]]; then
+    echo "Updating to r$REVISION..."
+    cd trunk
+    svn up -r $REVISION
+    make clean
+  else
+    "Cloning r$REVISION..."
+    svn co --username anon -r $REVISION svn://servers.simutrans.org/simutrans/trunk
+    cd trunk
+  fi
+
+  # Now inside trunk/
+  echo "Building r$REVISION..."
+  cp ../config.default ./
+  make
+  strip build/default/sim
+
+  # Exiting trunk/
+  cd ..
 fi
-
-# Now inside trunk/
-echo "Building r$REVISION..."
-cp ../config.default ./
-make
-strip build/default/sim
-
-# Exiting trunk/
-cd ..
 
 # Copy the game files
 echo "Copying game files..."
@@ -40,5 +42,8 @@ cp trunk/get_lang_files.sh $install_dir/simutrans/text/
 cd $install_dir/simutrans/text/
 ./get_lang_files.sh
 cd ../../../
+
+# Save last revision
+echo $REVISION > last_build.revision
 
 echo "Done installing r$REVISION!"
