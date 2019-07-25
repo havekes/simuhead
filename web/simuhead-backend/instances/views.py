c61ad3c8a01c613fe692a3df57f447b06d5718ff
Greg Havekes <greg@havek.es>
2019-07-25 15:20:07 -0400

    ADD remove pak file when removing form model

--- web/simuhead-backend/instances/views.py
+++ web/simuhead-backend/instances/views.py
@@ -1,3 +1,4 @@
+import os
 from rest_framework import viewsets
 from .serializers import PakSerializer, SaveSerializer, InstanceSerializer
 from .models import Pak, Save, Instance
@@ -7,10 +8,22 @@ class PakViewSet(viewsets.ModelViewSet):
     serializer_class = PakSerializer
     queryset = Pak.objects.all()
 
+    def destroy(self, request, *args, **kwargs):
+        """Remove the pak file before removing from the model"""
+        pak = self.get_object()
+        os.remove(pak.file.path)
+        return super(PakViewSet, self).destroy(request, *args, **kwargs)
+
 
 class SaveViewSet(viewsets.ModelViewSet):
     serializer_class = SaveSerializer
     queryset = Save.objects.all()
+    
+    def destroy(self, request, *args, **kwargs):
+        """Remove the save file before removing from the model"""
+        save = self.get_object()
+        os.remove(save.file.path)
+        return super(SaveViewSet, self).destroy(request, *args, **kwargs)
 
 
 class InstanceViewSet(viewsets.ModelViewSet):
