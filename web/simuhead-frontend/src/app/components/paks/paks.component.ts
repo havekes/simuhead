9b4275a230b7a4a1fb173056219bf155eb789511
Greg Havekes <greg@havek.es>
2019-07-27 18:15:02 -0400

    ADD save and pak components fully functional

--- web/simuhead-frontend/src/app/components/paks/paks.component.ts
+++ web/simuhead-frontend/src/app/components/paks/paks.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ApiService, FileInfo} from '../../api.service';
+import {ApiService, FileInfo, sortFileInfo} from '../../api.service';
 import {MatDialog, MatSnackBar} from '@angular/material';
 import {FileEditDialogComponent} from '../file-edit-dialog/file-edit-dialog.component';
 import {ConfirmDialogComponent} from '../confirm-dialog/confirm-dialog.component';
@@ -27,15 +27,27 @@ export class PaksComponent implements OnInit {
   private list() {
     this._apiService.paksList().subscribe({
       error: err => this._errorSnack.open(err.message, ERROR_SNACK_ACTION, ERROR_SNACK_CONFIG),
-      next: paks => this.paks = paks
+      next: paks => this.paks = paks,
+      complete: () => this.paks.sort(sortFileInfo)
     });
   }
 
-  openCreateDialog() {
+  openCreateDialog(title: string) {
     let createDialogRef = this._editDialog.open(FileEditDialogComponent, {
-      data: {new: true, file: <FileInfo>{}},
+      data: {title: title, file: <FileInfo>{}},
       disableClose: true,
       width: '400px',
+    });
+    createDialogRef.afterClosed().subscribe(data => {
+      if (data != undefined) {
+        this._apiService.pakPost(data).subscribe({
+          error: err => this._errorSnack.open(err.message, ERROR_SNACK_ACTION, ERROR_SNACK_CONFIG),
+          complete: () => {
+            this.list();
+            createDialogRef.close();
+          }
+        });
+      }
     });
   }
 
--- web/simuhead-frontend/src/app/components/saves/saves.component.ts
+++ web/simuhead-frontend/src/app/components/saves/saves.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService, FileInfo, sortFileInfo} from '../../api.service';
+import {MatDialog, MatSnackBar} from '@angular/material';
+import {FileEditDialogComponent} from '../file-edit-dialog/file-edit-dialog.component';
+import {ConfirmDialogComponent} from '../confirm-dialog/confirm-dialog.component';
+
+const ERROR_SNACK_ACTION = 'OK';
+const ERROR_SNACK_CONFIG = {
+  duration: 5000
+};
 
 @Component({
   selector: 'app-saves',
@@ -7,9 +16,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SavesComponent implements OnInit {
 
-  constructor() { }
+  saves: FileInfo[];
 
-  ngOnInit() {
+  constructor(private _apiService: ApiService,
+              private _fileDialog: MatDialog,
+              private _confirmDialog: MatDialog,
+              private _errorSnack: MatSnackBar) {
   }
 
+  private list() {
+    this._apiService.savesList().subscribe({
+      error: err => this._errorSnack.open(err.message, ERROR_SNACK_ACTION, ERROR_SNACK_CONFIG),
+      next: saves => this.saves = saves,
+      complete: () => this.saves.sort(sortFileInfo)
+    });
+  }
+
+  openCreateDialog(title: string) {
+    let createDialogRef = this._fileDialog.open(FileEditDialogComponent, {
+      data: {title: title, file: <FileInfo>{}},
+      disableClose: true,
+      width: 'auto',
+    });
+    createDialogRef.afterClosed().subscribe(data => {
+      if (data != undefined) {
+        this._apiService.savePost(data).subscribe({
+          error: err => this._errorSnack.open(err.message, ERROR_SNACK_ACTION, ERROR_SNACK_CONFIG),
+          complete: () => {
+            this.list();
+            createDialogRef.close();
+          }
+        });
+      }
+    });
+  }
+
+  deleteConfirmDialog(i, prompt) {
+    let confirmDialog = this._confirmDialog.open(ConfirmDialogComponent, {
+      data: prompt,
+    });
+    confirmDialog.afterClosed().subscribe((answer) => {
+      if (answer) {
+        this._apiService.saveDelete(this.saves[i]).subscribe({
+          error: err => this._errorSnack.open(err.message, ERROR_SNACK_ACTION, ERROR_SNACK_CONFIG),
+          complete: () => {
+            this.list();
+            confirmDialog.close();
+          }
+        });
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.list();
+  }
 }
